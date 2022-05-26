f067f1c5fce9a13de1f51b1c50ef95631be3a56d
Alfikri02 <Muhammadalfikri2002@gmail.com>
2022-05-26 14:12:35 +0700

    Desain Fragment Login

--- app/src/main/java/com/capstone/caltrack/MainActivity.kt
+++ app/src/main/java/com/capstone/caltrack/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.capstone.caltrack
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.caltrack.databinding.ActivityMainBinding
 
